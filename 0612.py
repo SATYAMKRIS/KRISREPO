b1caba65efac5922f8bf6b9448757040641e6077
SATYAMKRIS <115940988+SATYAMKRIS@users.noreply.github.com>
2023-05-20 00:40:10 +0530

    Add files via upload
    THIS PYTHON CODE REVERSE THE GIVEN ARRAY

---
+++ 0612.py
@@ -0,0 +1,16 @@
+a = [4, 7, 9, 11, 22, 44, 59]
+
+print('Printing the List Items in Rererse')
+print(a[::-1])
+
+a.reverse()
+print('\nPrinting the List Items in Rererse')
+print(a)
+
+number = int(input("Number: "))
+sum = 0
+for i in range(1, number + 1):
+    sum += i
+print(sum)
+
+
